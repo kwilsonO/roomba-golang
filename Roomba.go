6899f7f348522f483878f02de9386b62afe73260
kwilsonO <kylewilson52@gmail.com>
2016-04-11 17:42:02 -0700

    added parsing beginning dijkstras? i think is right

--- Roomba.go
+++ Roomba.go
@@ -1,17 +1,61 @@
-import fmt
-import "text/template/parse"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 
+type Vertex struct{
+	X, Y int
+	dist int
+	prev int
+}
+
+var verts []Vertex
 func main() {
 
-	parseInput()
+	if len(os.Args) != 2 {
+		fmt.Println("Send a file path noob")
+	}
 
+	lines, err := ParseLines(os.Args[1], func(s string)(string,bool){
+		return s, true
+	})
 
+	if err != nil {
+		fmt.Println("you're file format sucks", err);
+		return
+	}
+
+	verts = make(map[Vertex]int)
+
+	for _, 1 := range lines {
+		//do some data handling
+	}
 }
 
 
 
-func parseInput(){
+func ParseLines(filePath string, parse func(strin) (string, bool)){
+	inputFile, err = os.Open(filePath);
+	if err != nil {
+		return nil, err
+	}
 
+	defer inputFile.close()
+
+	scanner := bufio.NewScanner(inputFile);
+	var results []string
+	for scanner.Scan() {
+		if output, add := parse(scanner.Text()); add {
+			results.append(results, output)
+		}
+	}
+
+	if err  := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return results, nil
 
 }
